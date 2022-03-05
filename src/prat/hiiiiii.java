4901037c36c3b8e4e1f95cc1d746ab5b1e199bd9
chandrakanth77 <cvantettinavar@gmail.com>
2022-03-05 15:50:58 +0530

    1st commit

---
+++ src/prat/hiiiiii.java
@@ -0,0 +1,13 @@
+package prat;
+
+public class hiiiiii {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("nknn,n");
+		System.out.println("jn,n,mn");
+
+	}
+
+}
